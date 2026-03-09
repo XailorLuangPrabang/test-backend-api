bc09b4ebbb9bd15cc33d861be7bb34f9f55c4128
Sai lor <saiylor437@gmail.com>
2026-03-09 21:02:37 +0700

    fix db env

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,15 @@
 import { DataSource } from "typeorm";
-import { User } from "./entities/user";
-import { Product } from "./entities/product";
 import { Order } from "./entities/order";
+import { Product } from "./entities/product";
+import { User } from "./entities/user";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "root123",
+    database: process.env.DB_NAME || "back_end_db",
     entities: [User, Product, Order],
     synchronize: true,
 });
